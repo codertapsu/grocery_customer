d43ab03e22fd825ccac06d3ec11d08e7b9a92d41
Khanh Hoang <hoangduykhanh21@gmail.com>
2022-08-30 23:01:32 +0700

    Configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+const path = require('path');
+const runtimeCaching = require('next-pwa/cache');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
+};
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+});
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('tailwindcss-safe-area'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/line-clamp'),
+	],
+};
